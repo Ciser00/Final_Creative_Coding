7aeeaeb62fcf08c12527fccc4de84e12de867395
Ciser00 <gpc281@nyu.edu>
2022-04-17 22:56:44 -0400

    Create final doc .vb

---
+++ final doc .vb
@@ -0,0 +1,36 @@
+
+
+Free Falling Visual (easier to view on google docs : https://docs.google.com/document/d/1bTgQp7T0vNb5fHXmdUBrgGg0M1Sx0XFMrSUCBCu7A54/edit?usp=sharing )
+
+	I have always wanted to try and make some interesting visual patterns with my code but have never taken the time to try it. I wanted to attempt to make some sort of “trippy” visual project for this final but a lot of the effects that I had researched looked more texture based than object based. I looked at a couple of idea videos from 3ds projects and games and I stumbled upon this video “https://youtu.be/y2IAjXl5xRU?t=100” and at the time stamp I saw a very studying visual. That visual is what I want the base of my project to be. I want to create this free falling visual with a bit more flare, this could even be used as the background for my portfolio website. 
+
+
+Project break up: 
+	
+First steps
+Create the shapes I would use for the project 
+Basic shapes 
+Spheres 
+
+More complex shapes 
+Devil fruit inspired shapes 
+3d models of them might exist
+Random objects that have a similar feel
+Real fruits 
+
+
+
+Next steps 
+Creating the procedure in which the shapes and models are displayed 
+General idea is that there is a set radius of space where things cannot spawn and from that space the objects and basic shapes will be generated
+The camera will move through this negative space into the void
+Shapes should be deleted when they pas a certain point and the spawn distance should be keep low to not have the program lag 
+This will be the hardest part of the project and take the majority of time
+Final steps 
+Clean up
+Check code for errors 
+Add extra shapes or objects 
+Correct or add missing comments
+
+
+
