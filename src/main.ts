28a3eca184a08ed59e5514f2c4cc43ccd9aaf9e3
Ciser00 <gpc281@nyu.edu>
2022-05-01 19:29:28 -0400

    Shapes added / gui started

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { ShaderMaterial, Shading } from 'three';
 import { gsap } from "gsap";
+import * as dat from 'dat.gui';
 
 let renderer: THREE.WebGLRenderer;
 let scene: THREE.Scene;
@@ -26,8 +27,9 @@ let controls: OrbitControls;
 let stats: any;
 let sphere: THREE.Mesh;
 let cube: THREE.Mesh;
-let scube: THREE.Mesh;
-let plane: THREE.Mesh;
+let cylinder: THREE.Mesh;
+let cone : THREE.Mesh;
+let plane : THREE.Mesh;
 let group: THREE.Group;
 let standModel: THREE.Object3D;
 let exampleTexture: THREE.Texture;
@@ -99,6 +101,52 @@ class Sphere1 extends Movers{
     }  
 }
 
+class Cylinder1 extends Movers{
+    geometryCylinder : any;
+    materialCylinder : any;
+    radiusTop : number;
+    radiusBot : number;
+    height : number;
+    radialSegments : number;
+    constructor(xpos: number, ypos: number, zpos:number, rotation:number,radiusTop : number,radiusBot : number,height : number, radialSegments : number){
+        super(xpos, ypos, zpos, rotation);   
+        this.radiusTop = radiusTop;
+        this.radiusBot = radiusBot;
+        this.height = height;
+        this.radialSegments = radialSegments;
+        this.geometryCylinder = new THREE.CylinderGeometry(this.radiusTop, this.radiusBot, this.height, this.radialSegments  );
+        this.materialCylinder = new THREE.MeshPhongMaterial({ color: 0x710B0B });
+    }      
+}
+class Cone1 extends Movers {
+    geometryCone : any;
+    materialCone : any;
+    radius: number;
+    height : number;
+    radialSegments : number;
+    constructor(xpos: number, ypos: number, zpos:number, rotation:number,radius : number,height : number, radialSegments : number){
+        super(xpos, ypos, zpos, rotation);   
+        this.radius = radius;
+        this.height = height;
+        this.radialSegments = radialSegments;
+        this.geometryCone = new THREE.ConeGeometry(this.radius, this.height, this.radialSegments  );
+        this.materialCone = new THREE.MeshPhongMaterial({ color: 0x710B0B });
+    }
+}
+
+class Plane1 extends Movers{
+    geometryPlane : any;
+    materialPlane : any;
+    height :number;
+    width : number;
+    constructor(xpos: number, ypos: number, zpos:number, rotation:number,height : number, width:number){
+        super(xpos, ypos, zpos, rotation);   
+        this.height = height;
+        this.width = width;
+        this.geometryPlane = new THREE.PlaneGeometry( this.height, this.width );
+        this.materialPlane = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
+    }
+}
 
 
 
@@ -157,6 +205,17 @@ function initScene() {
     lightPoint.shadow.camera.near = cameraNear;
     lightPoint.shadow.camera.far = cameraFar;
 
+
+    const gui = new dat.GUI()
+    let left_gui = gui.addFolder("Left")
+    left_gui.add(controller,"left",0,1)
+
+    let right_gui = gui.addFolder("Right")
+    right_gui.add(controller,"right",0,1)
+
+
+    
+    
     for(let i=0; i < 7; i++){   //each cube will have a length 5 longer than the last one, 6 total 
         let temp = new Cube1(0,0,i-100,0,3,3,cube_length) // x,y,z,rotationg,legth,width,height   ///now i can create a whole bnch of diffrent types of cube objs, just have to know their index     
         obj_lst.push(temp);
@@ -235,7 +294,7 @@ function animate() {
     requestAnimationFrame(() => {
         animate();  
     });
-    //scene.background = new THREE.Color(0xffffff);
+    //scene.background = new THREE.Color(0xffffff);   customize background color 
 
     x+=0.5
     timer += 1   
@@ -269,17 +328,27 @@ function placeCube(x: {}){
         scene.remove(cube)   //not working
     }
 }
-
 function placeSphere(x: {}){
-
     sphere = new THREE.Mesh(x.geometrySphere, x.materialSphere);
     sphere.castShadow=true;
     sphere.position.set(x.xpos,x.ypos,x.zpos)
     //sphere.scale.set(.2,.2,.2)
     scene.add(sphere);
-  
 }
 
+function placeCylinder(x:{}){
+    cylinder = new THREE.Mesh( x.geometryCylinder, x.materialCylinder );
+    scene.add( cylinder ); 
+}
+function placeCone(x:{}){
+    cone = new THREE.Mesh( x.geometryCone, x.materialCone );
+    scene.add( cone );
+} 
+
+function placePlane(x:{}){
+    plane = new THREE.Mesh( x.geometryPlane, x.materialPlane );
+    scene.add( plane );
+}
 function clear(){
   //  scene.remove(cube)  //not working 
 }
@@ -288,7 +357,7 @@ function clear(){
 
 function leftControl(){
     placeCube(obj_lst[controller.left_counter])
-    gsap.fromTo(cube.position,{x:4},{x:4,z:50,duration: controller.left_duration, onComplete: clear})
+    gsap.fromTo(cube.position,{x:-4},{x:-4,z:50,duration: controller.left_duration, onComplete: clear})
     if (controller.left_counter == obj_lst.length -1 ){
         controller.left_counter = 1;
     }else {
@@ -300,7 +369,7 @@ function rightControl(){
     switch (controller.right) {
         case 0:   //customizable shape spawned 
             placeCube(obj_lst[controller.right_counter])
-            gsap.fromTo(cube.position,{x:-4},{x:-4,z:50,duration: controller.right_duration, onComplete: clear})   //customizable left and right spacing 
+            gsap.fromTo(cube.position,{x:4},{x:4,z:50,duration: controller.right_duration, onComplete: clear})   //customizable left and right spacing 
             if (controller.right_counter == obj_lst.length -1 ){
                 controller.right_counter = 1;
             }else {
@@ -309,7 +378,7 @@ function rightControl(){
             break;
         case 1:  //could do dromto / tofrom for  a reverse mode 
             placeSphere(sphere_lst[controller.right_counter])
-            gsap.fromTo(sphere.position,{x:-4},{x:-4,z:50,duration: controller.right_duration, onComplete: clear})   //customizable left and right spacing 
+            gsap.fromTo(sphere.position,{x:4},{x:4,z:50,duration: controller.right_duration, onComplete: clear})   //customizable left and right spacing 
             if (controller.right_counter == sphere_lst.length -1 ){
                 controller.right_counter = 1;
             }else {
@@ -354,4 +423,14 @@ function placeTest(){
 main()
 //randomize colors when creating shapes 
 //add more shapes ?
-//
+
+
+//custmoizable features added 
+    // -through gui 
+    // -add as many as possible 
+//background options 
+    // -change color 
+    // -add anamations ?
+    //     -spehers in background floatin aorund ?
+    //     -wall of rects 
+//add more shapes 
